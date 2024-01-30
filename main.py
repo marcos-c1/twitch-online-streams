35c88d54936283e71052aafaf6c7f09f84f113fe
marcos-c1 <marcos.b.campos14@gmail.com>
2024-01-30 18:21:03 -0300

    feat: get and save fn to manipulate access token

--- main.py
+++ main.py
@@ -1,29 +1,66 @@
 import requests
-import os
+from os import path
+import asyncio
 
 def get_client_secret():
     client_id = ""
     client_secret = ""
-    with open(os.path.expanduser('~/.secret'), "r") as f:
-        client_id = f.readline()
-        client_secret = f.readline()        
+    with open(path.expanduser('~/.secret'), "r") as f:
+        client_id = f.readline()[:-1]
+        client_secret = f.readline()[:-1]        
 
     f.close()
     return client_id, client_secret 
 
-def twitch_auth(client_id: str, client_secret: str):
-    headers = dict()
-    data = dict() 
-    headers['content-type'] = 'application/x-www-form-urlencoded'
-    data['client_id'] = client_id 
-    data['client_secret'] = client_secret
-    data['grant_type'] = 'client_credentials'
+def save_token(token: str):
+    with open(path.expanduser('./token'), "w") as f:
+        f.write(token)
+    f.close()
+
+def get_access_token():
+    access_token = ""
+    if(path.exists('./token')):
+        with open(path.expanduser('./token'), "r") as f:
+            access_token = f.readline()
+
+    if not access_token:
+        client_id, client_secret = get_client_secret()
+        response = twitch_auth_flow(client_id, client_secret)
+        access_token = response["access_token"]
+        return access_token
+    return access_token
+
+def twitch_auth_flow(client_id: str, client_secret: str):
+    headers = {
+            'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    data = {
+            'client_id': client_id,
+            'client_secret': client_secret,
+            'grant_type': 'client_credentials'
+    }
 
     response = requests.post('https://id.twitch.tv/oauth2/token', headers=headers, data=data).json()
-    return response
+    
+    match response.status_code:
+        case 200:
+            try:
+                save_token(response['access_token'])
+            except e:
+                raise Exception(e)
+            finally:
+                return response
+        case 401:
+            # TODO: Generate another access token and store.
+            raise Exception("Token expired.")
+        case 500:
+            raise Exception("Server error.") 
+        case _:
+            raise Exception(f"{response.status_code}: {response.content}")
 
-if __name__ == '__main__':
-    client_id, client_secret = get_client_secret()
-    response = twitch_auth(client_id, client_secret)
-    access_token = response["access_token"]
-    print(access_token)
+def main():
+    if __name__ == '__main__':
+        access_token = get_access_token()
+        print(access_token)
+
+main()
