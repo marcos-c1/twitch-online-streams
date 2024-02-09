e375a734e2d7b64591d689343bc15f10b4e91e58
marcos-c1 <marcos.b.campos14@gmail.com>
2024-02-09 12:38:03 -0300

    feat: make listview works with channels endpoint

--- main.py
+++ main.py
@@ -1,26 +1,73 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Header, Footer
+from textual.widgets import Header, Footer, Label, ListView, ListItem, Static
+from textual.containers import Horizontal, Vertical, ScrollableContainer
+from textual.binding import Binding
 from controllers.channel_controller import ChannelController 
+from threading import Thread
+from time import sleep
 
 # https://textual.textualize.io/
-class StopwatchApp(App):
-    """A Textual app to manage stopwatches."""
+class ListStream(Static):
 
-    BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
+    LIST_STREAM_CHANNELS = []
+   
+    def __init__(self):
+        super().__init__()
+        self.data = self._request_data() 
+        channels = self.data 
+        for index, ch in enumerate(channels):
+            
+            item = ListItem(Label(f"{ch.user_name}\n{ch.title}\n{ch.game_name}\n{ch.viewer_count}", id=f'label{i}'))
+            self.LIST_STREAM_CHANNELS.append(item)
+        #Thread(target=self._channels_loop, daemon=True).start()
+                                               
+    def _channels_loop(self) -> None:
+         while True:
+            self.data = self._request_data()
+            self.update_channels()
+            sleep(1000)
+
+    def update_channels(self) -> None:
+        channels = self.data
+        for ch in channels:
+            item = ListItem(Label(f"{ch.user_name}\n{ch.title}\n{ch.game_name}\n{ch.viewer_count}")),
+            self.LIST_STREAM_CHANNELS.append(item)
+        
+    def _request_data(self): 
+        ch_controller = ChannelController()
+        channels = ch_controller.get_followed_channels('rvlt1')
+        return channels
 
     def compose(self) -> ComposeResult:
         """Create child widgets for the app."""
-        yield Header()
-        yield Footer()
+        yield ListView(
+            *self.LIST_STREAM_CHANNELS,
+            id='list',
+        )
 
+class StreamApp(App):
+    """A Textual app to manage stopwatches."""
+
+    CSS_PATH = "./css/app.tcss"
+
+    BINDINGS = [
+            ("d", "toggle_dark", "Toggle dark mode"),
+            ("q", "quit", "Quit"),
+    ]
+
+    def compose(self) -> ComposeResult:
+        """Create child widgets for the app."""
+        yield ListStream()
+        yield Footer()
+        
     def action_toggle_dark(self) -> None:
         """An action to toggle dark mode."""
         self.dark = not self.dark
 
-
 if __name__ == "__main__":
-    #app = StopwatchApp()
-    #app.run()
+    app = StreamApp()
+    app.run()
+    """
     ch_controller = ChannelController()
     channels = ch_controller.get_followed_channels('rvlt1')
     
@@ -32,5 +79,5 @@ if __name__ == "__main__":
         print(f'{ch.started_at}')
         print(f'{ch.language}')
         print()
-
+    """
     #api.validate_token()
