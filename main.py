01c80b452179bd454b1860c803aed5a3289a542e
marcos-c1 <marcos.b.campos14@gmail.com>
2024-02-14 18:12:00 -0300

    feat: change listview to datatable widget

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Header, Footer, Label, ListView, ListItem, Static, Select
+from typing import Any, Callable, ClassVar, Generic, Iterable, NamedTuple, TypeVar, cast
+from textual.widgets import Header, Footer, Label, ListView, ListItem, Static, Select, Log, DataTable
 from textual.containers import Horizontal, Vertical, ScrollableContainer
-from textual.binding import Binding
+from textual.binding import Binding, BindingType
 from textual import on
+from textual.reactive import reactive
 from controllers.channel_controller import ChannelController 
+from rich.text import Text, TextType
 from threading import Thread
 from time import sleep
+from datetime import datetime
+from zoneinfo import ZoneInfo
 
-LIST_STREAM_CHANNELS = []
+LIST_STREAM_CHANNELS = [
+    ("User", "Title", "Category", "Uptime", "Views", "Language")
+]
 LIST_STREAM_CATEGORIES = []
 DICT_STREAM = [] 
 
@@ -27,75 +34,56 @@ dict keys:
     "thumbnail_url"
 """
 
-def append_to_list_from_set(s: set) -> None:
-    for elem in s:
-        LIST_STREAM_CATEGORIES.append(elem)
-
 # https://textual.textualize.io/
 class SelectCategories(Static):
 
-    def compose(self) -> ComposeResult:
-        yield Select((category, category) for category in LIST_STREAM_CATEGORIES)
+    categories = set() 
+
+    def __init__(self) -> None:
+        super().__init__()
+        for row in LIST_STREAM_CHANNELS:
+            self.categories.add(row[2])
 
     @on(Select.Changed)
     def select_changed(self, event: Select.Changed) -> None:
         self.title = str(event.value)
 
-class ListStream(Static):
+    def compose(self) -> ComposeResult:
+        # TODO: Use the same object as DataTable use to filter by category
+        """
+            Probably will solve the problem
+        """
+        yield Select((category, category) for category in self.categories)
 
-    def __init__(self):
-        super().__init__()
-        self.data = self._request_data() 
-        channels = self.data 
-        categories_set = set()
-        for ch in channels:
-            """
-            dct_str['id'] = ch.id
-            dct_str['user_id'] = ch.user_id
-            dct_str['user_login'] = ch.user_login
-            dct_str['user_name'] = ch.id
-            dct_str['game_id'] = ch.id
-            dct_str['game_name'] = ch.id
-            dct_str['type'] = ch.id
-            dct_str['title'] = ch.id
-            dct_str['viewer_count'] = ch.id
-            dct_str['started_at'] = ch.id
-            dct_str['language'] = ch.id
-            dct_str['thumbnail_url'] = ch.id
-            dct_str['tag_ids'] = ch.id
-            dct_str['tags'] = ch.id
-            dct_str['is_mature'] = ch.id
-           """ 
-            DICT_STREAM.append(ch._get_payload())
-            item = ListItem(Label(f"{ch.user_name}"))
-            LIST_STREAM_CHANNELS.append(item)
-            categories_set.add(f"{ch.game_name}")
-        append_to_list_from_set(categories_set)
-        #Thread(target=self._channels_loop, daemon=True).start()
-                                               
-    def _channels_loop(self) -> None:
-         while True:
-            self.data = self._request_data()
-            self.update_channels()
-            sleep(1000)
+class DataList(Static):
 
-    def update_channels(self) -> None:
-        channels = self.data
-        for ch in channels:
-            item = ListItem(Label(f"{ch.user_name}\n{ch.title}\n{ch.game_name}\n{ch.viewer_count}")),
-            LIST_STREAM_CHANNELS.append(item)
-        
-    def _request_data(self): 
-        ch_controller = ChannelController()
-        channels = ch_controller.get_followed_channels('rvlt1')
-        return channels
+    BINDINGS: list[BindingType] = [
+        Binding("enter", "select_cursor", "Select", show=False),
+        Binding("k", "cursor_up", "Cursor Up", show=False),
+        Binding("j", "cursor_down", "Cursor Down", show=False),
+        Binding("right", "cursor_right", "Cursor Right", show=False),
+        Binding("left", "cursor_left", "Cursor Left", show=False),
+        Binding("k", "page_up", "Page Up", show=False),
+        Binding("j", "page_down", "Page Down", show=False),
+    ]
+
+    #Thread(target=self._channels_loop, daemon=True).start()
+
+    def on_mount(self) -> None:
+        table = self.query_one(DataTable)
+        table.BINDINGS = self.BINDINGS
+        table.header_height = 2
+        table.cursor_type = "row" 
+        table.zebra_stripes = True
+        table.add_columns(*LIST_STREAM_CHANNELS[0])
+        for index, value in enumerate(LIST_STREAM_CHANNELS[1:]):
+            table.add_row(*value, height=2, key=index)
 
     def compose(self) -> ComposeResult:
         """Create child widgets for the app."""
         yield SelectCategories() 
-        yield ListView(
-            *LIST_STREAM_CHANNELS,
-            id='list',
+        yield DataTable(
+            id='dt',
         )
 
 class StreamApp(App):
@@ -108,14 +96,48 @@ class StreamApp(App):
             ("q", "quit", "Quit"),
     ]
 
+    def __init__(self) -> None:
+        super().__init__()
+        streams = self._get_followed_channels() 
+                                                                                                          
+        for ch in streams:
+            """
+            channel object attributes:
+                "id"
+                "user_id"
+                "user_login"
+                "user_name"
+                "game_id"
+                "game_name"
+                "type"
+                "title"
+                "viewer_count"
+                "started_at"
+                "language"
+                "thumbnail_url"
+            """
+            #datetime_str = '09/19/22T13:55:26Z'
+            now_utc = datetime.now(ZoneInfo('UTC')).replace(tzinfo=None)
+            started_at = datetime.strptime(ch.started_at, "%Y-%m-%dT%H:%M:%SZ")
+            diff = (now_utc - started_at)
+            diff_str = ''.join(str(diff)).split('.')[0]
+            CHANNEL = (ch.user_name, ch.title, ch.game_name, diff_str, ch.viewer_count, ch.language)
+            LIST_STREAM_CHANNELS.append(CHANNEL)
+
     def compose(self) -> ComposeResult:
         """Create child widgets for the app."""
-        yield ListStream()
-        yield Footer()
+        with Vertical():
+            yield DataList()
+            yield Footer()
         
     def action_toggle_dark(self) -> None:
         """An action to toggle dark mode."""
         self.dark = not self.dark
+
+    def _get_followed_channels(self): 
+        ch_controller = ChannelController()
+        channels = ch_controller.get_followed_channels('rvlt1')
+        return channels
 
 if __name__ == "__main__":
     app = StreamApp()
